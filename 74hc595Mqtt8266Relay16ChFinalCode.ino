1ce5f3b134b890b7ea200697da315ac6c5aee3a0
EdIVeDeR <36791310+EdIVeDeR@users.noreply.github.com>
2023-02-24 15:25:48 -0500

    Update 74hc595Mqtt8266Relay16ChFinalCode.ino

--- 74hc595Mqtt8266Relay16ChFinalCode.ino
+++ 74hc595Mqtt8266Relay16ChFinalCode.ino
@@ -7,17 +7,20 @@ const char* mqttServer =          "192.168.1.100";      // MQTT server adress
 const int   mqttPort =            1883;                 // MQTT server Port
 const char* mqttUser =            "your_MQTT_username"; // MQTT server username
 const char* mqttPassword =        "your_MQTT_password"; // MQTT server password
-const char* topicIn =             "16ChRelay1In";       // MQTT Topic sending to the relays board
-const char* topicOut =            "16ChRelay1Out";      // MQTT Topic Sending from the relay board
+const char* topicIn =             "16ChRelay1In";       // MQTT Topic in
+const char* topicOut =            "16ChRelay1Out";      // MQTT Topic in
 const int   latchPin =            12;                   // Latch Pin of 74hc595
 const int   clockPin =            13;                   // Clock  Pin of 74hc595
 const int   dataPin =             14;                   // Data  Pin of 74hc595
 int         oePin =               5;                    // Oe Pin of 74hc595  
 const int   numRegisters =        2;                    // Number of 74hc595 on the board
 const int   numRelays =           numRegisters * 8;     // Number of relay on the board obtain by multiplication of the number of relay by the number of 74hc595 on the board
-bool        relayState[numRelays];                      // Store the state of each relay
+bool        relayState            [numRelays];          // Store the state of each relay
 int         connectionAttempts =  0;                    // Number of connection try for WiFi
 int         failedAttempts =      0;                    // Number of connection try for MQTT server
+IPAddress   subnet                (255, 255, 0, 0);		  // Subnet Mask
+IPAddress   gateway               (192, 168, 1, 1);		  // Default Gateway
+IPAddress   local_IP              (192, 168, 1, 184);	  // Static IP Address for ESP8266
 
 
 WiFiClient espClient;
@@ -98,20 +101,18 @@ void mqttCallback(char *topic, byte *payload, unsigned int length) {
   // Set the state of the relay if a valid command was received
   if (relayNum >= 1 && relayNum <= numRelays) {
     setRelayState(relayNum - 1, newState);
-  }
+  }  
 }
 
-
-
 void setRelayState(int relayNum, bool state) {
-  
-  relayState[relayNum] = state; // Set the state of a relay
-  byte outputData[numRegisters];// Update the shift registers with the new relay state
+  // Set the state of a relay
+  relayState[relayNum] = state;
 
+  // Update the shift registers with the new relay state
+  byte outputData[numRegisters];
   for (int i = 0; i < numRegisters; i++) {
     outputData[i] = 0;
   }
-
   for (int i = 0; i < numRelays; i++) {
     int regNum = i / 8;
     int bitNum = i % 8;
@@ -124,7 +125,15 @@ void setRelayState(int relayNum, bool state) {
     shiftOut(dataPin, clockPin, MSBFIRST, outputData[i]);
   }
   digitalWrite(latchPin, HIGH);
+
+  // Publish the confirmation message
+  String message = String(relayNum) + " " + (state ? "ON" : "OFF");
+  client.publish("16ChRelay1Out", message.c_str());
+
+// Log the confirmation message to Serial for debugging
+  Serial.println("Relay state changed: " + message);
 }
+
 
 void setup() {
 
@@ -136,8 +145,15 @@ void setup() {
   pinMode(oePin, OUTPUT);
   digitalWrite(oePin, LOW);  // enable outputs
 
-  WiFi.begin(ssid, password);
-while (WiFi.status() != WL_CONNECTED) {
+  if (WiFi.config(local_IP, gateway, subnet)) {
+     Serial.println("Static IP Configured");
+  }
+  else {
+    Serial.println("Static IP Configuration Failed");
+  }
+
+WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
 delay(500);
 Serial.println("Connecting to WiFi...");
 connectionAttempts++;
@@ -149,8 +165,7 @@ ESP.restart(); // reset board
 }
 
 Serial.println("Connected to WiFi"); // if connected before reaching 50 attempts, print success message.
-
-  client.setServer(mqttServer, mqttPort);
+client.setServer(mqttServer, mqttPort);
 client.setCallback(mqttCallback);
 
 while (!client.connected()) {
@@ -169,18 +184,14 @@ while (!client.connected()) {
 }
 
 Serial.println("Connected to MQTT server");
-client.publish(topicOut,"16 Channels Relay Board #1 is connect and is listening on Topic 16ChRelayIn");
+client.publish(topicOut,"16 Channels Relay Board #1 is connect and listening on Topic 16ChRelay1In");
 
-
-
-  // Set all relays to off initially
+// Set all relays to off initially
   for (int i = 0; i < numRelays; i++) {
     setRelayState(i, false);
   }
 }
 
 void loop() {
-
 client.loop();
-   
 }
