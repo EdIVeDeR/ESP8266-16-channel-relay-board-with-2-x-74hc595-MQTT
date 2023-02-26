92f0fbdc66b8cb5c7fbbac2874afe0b132f5ff82
EdIVeDeR <36791310+EdIVeDeR@users.noreply.github.com>
2023-02-26 09:10:26 -0500

    Update 74hc595Mqtt8266Relay16ChFinalCode.ino

--- 74hc595Mqtt8266Relay16ChFinalCode.ino
+++ 74hc595Mqtt8266Relay16ChFinalCode.ino
@@ -1,8 +1,8 @@
 #include "EspMQTTClient.h"
 
-const char* clientId =            "SSID";               // Change this ID, to make it the only one with this name on your network
-const char* ssid =                "PASSWORD";           // Change this value with your SSID name
-const char* password =            "deepshrub409";       // Change this value with your Password
+const char* clientId =            "16ChRelay1";         // Change this ID, to make it the only one with this name on your network
+const char* ssid =                "SSID";               // Change this value with your SSID name
+const char* password =            "PASSWORD";           // Change this value with your Password
 const char* mqttServer =          "192.168.1.100";      // MQTT server adress
 const int   mqttPort =            1883;                 // MQTT server Port
 const char* mqttUser =            "your_MQTT_username"; // MQTT server username
@@ -17,6 +17,16 @@ const int   numRegisters =        2;                    // Number of 74hc595 on 
 const int   numRelays =           numRegisters * 8;     // Number of relay on the board obtain by multiplication of the number of relay by the number of 74hc595 on the board
 bool        relayState            [numRelays];          // Store the state of each relay
 
+// Setup different information and directive messages
+
+const String clientIdIn =                      String(clientId) + String(topicIn);                                                                                                                                                  // MQTT Topic in
+const String clientIdOut =                     String(clientId) + String(topicOut);                                                                                                                                                 // MQTT Topic out
+const String mqttCommandTopic =                "Send your commands to this topic " + clientIdIn;                                                                                                                                    // MQTT Topic to send command
+const String mqttDebugTopic =                  "For debuging and state relay update, subscribe to topic " + clientIdOut;                                                                                                                  // MQTT Topic to receive debug and relay state update information
+const String webUpdaterConnectionInformation = "Web updapter is active and can be access via internet browser at http://" + String(clientId) + ".local , user is :" + String(mqttUser) + " and password is " + String(mqttPassword); // Web updater web site information
+const String mqttConnectionConfirmation =      "16 Channels Relay Board is connect and listening on Topic " + clientIdOut +". Relay #1 is identify as relay 0 and #16 to relay 15";                                                       // MQTT message to confirme that board is connecte to Wifi and MQTT Broker
+const char*  mqttLastWillMessage =             (clientIdOut).c_str();                                                                                                                                                               // Conversion of clientIdOut to const char* for last will print debug
+
 // Setup WiFi and Mqtt client 
 
 EspMQTTClient client(
@@ -29,12 +39,16 @@ EspMQTTClient client(
   mqttPort      
 );
 
-// Function that change the relay state after a valid Mqtt command by  
+// Function that change the relay state after a valid Mqtt command by 
+
   void setRelayState(int relayNum, bool state) {
+
 // Set the state of a relay
+
   relayState[relayNum] = state;
 
 // Update the shift registers with the new relay state
+
   byte outputData[numRegisters];
   for (int i = 0; i < numRegisters; i++) {
     outputData[i] = 0;
@@ -54,18 +68,22 @@ EspMQTTClient client(
 
 
 // Publish the confirmation message
+
   String message = String(relayNum) + " " + (state ? "ON" : "OFF");
   
 // Log the confirmation message to Serial for debugging
+
   Serial.println("Relay state changed: " + message);
-  client.publish(String(clientId) + String(topicOut),message);
+  client.publish(clientIdOut,message);
 }
+
 // This function is called once everything is connected (Wifi and MQTT)
-// WARNING : YOU MUST IMPLEMENT IT IF YOU USE EspMQTTClient
+
 void onConnectionEstablished()
 {
-  client.publish(String(clientId) + String(topicOut), "16 Channels Relay Board is connect and listening on Topic "+String(clientId)+"In. Relay #1 is identify as relay 0 and #16 to 15"); // Publish a message to Topic put in variable topicOut. You can activate the retain flag by setting the third parameter to true
-  client.subscribe(String(clientId) + topicIn, [](const String & payload) { // Subscribe to topic insert in variable topicOut and display received message to Serial
+  client.publish(clientIdOut, mqttConnectionConfirmation);        // Publish a message to Topic put in variable topicOut. You can activate the retain flag by setting the third parameter to true
+  client.publish(clientIdOut, webUpdaterConnectionInformation);   // Publish to topic variable "clientIdOut" information to connect to the local web site where you can update both firmware and code
+  client.subscribe(clientIdIn, [](const String & payload) {       // Subscribe to topic insert in variable topicOut and display received message to Serial
   String str_msg = String(payload);
 
 // Extract command from string
@@ -106,7 +124,7 @@ if (str_msg.equals("ALL ON")) {
 } else if (str_msg.startsWith("Relay #")) {
 
   // Switch individual relays on or off
-  int relayNumber = str_msg.substring(7, 9).toInt(); // Extract relay number from string
+  int relayNumber = str_msg.substring(7, 9).toInt();                                                          // Extract relay number from string
   relayNumber = relayNumber - 1;
   bool status = str_msg.endsWith("ON");
   setRelayState(relayNumber, status);
@@ -122,8 +140,8 @@ if (str_msg.equals("ALL ON")) {
 void setup()
 {
   Serial.begin(115200);                                                                                       // Start serial monitor debug
-  Serial.println("Send your commands to this topic " + String(clientId) + String(topicIn));
-  Serial.println("For debuging and state relay update, subscribe to " + String(clientId) + String(topicOut));
+  Serial.println(mqttCommandTopic);
+  Serial.println(mqttDebugTopic);
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
@@ -133,8 +151,9 @@ void setup()
     setRelayState(i, false);}                                                                                 // Switch all relays off to start
   client.enableDebuggingMessages();                                                                           // Enable debugging messages sent to serial output
   client.enableHTTPWebUpdater();                                                                              // Enable the web updater. User and password default to values of MQTTUsername and MQTTPassword. These can be overridded with enableHTTPWebUpdater("user", "password").
-  client.enableOTA();                                                                                         // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
-  client.enableLastWillMessage((String(clientId) + String(topicOut)).c_str(), "I am going offline");          // You can activate the retain flag by setting the third parameter to true
+  Serial.println(webUpdaterConnectionInformation);                                                            // Send to serial monitor information to connect to the local web site where you can update both firmware and code
+  //client.enableOTA();                                                                                       // Enable OTA (Over The Air) updates. Password defaults to MQTTPassword. Port is the default OTA port. Can be overridden with enableOTA("password", port).
+  client.enableLastWillMessage(mqttLastWillMessage, "I am going offline");                                    // You can activate the retain flag by setting the third parameter to true
 }
 
 void loop()
